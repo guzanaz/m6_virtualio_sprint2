e68556daba1d50e25735f1eaebe7d2c764e3001b
Daniela <daniela.gallardo.reyes@gmail.com>
2022-02-13 17:59:51 +0100

    axios import and implementation working
    working with external database (not virtualio)

--- src/main.js
+++ src/main.js
@@ -4,19 +4,18 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import App from './App.vue';
-import axios from 'axios';
-// Add it to Vue prototype (use any variable you like, preferrably prefixed with a $).
-Vue.prototype.$http = axios;
+
+
 
 import VueRouter from 'vue-router';
 import { createRouter } from './router';
 import { createStore } from './store';
 
-Vue.use(Vuex)
-Vue.use(VueRouter)
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-Vue.use(axios)
+Vue.use(Vuex);
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
 
 Vue.config.productionTip = false
 
--- src/views/pruebaAPI.vue
+++ src/views/pruebaAPI.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="content"></div>
+  <div v-if="result" class="content">
+    <p>User ID: {{result.userId}}</p>
+    <p>Title: {{result.title}}</p>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
+  data: () => ({
+    result: null
+  }),
   created() {
-    axios.get("http://127.0.0.1:8000/").then((result) => {
-      console.log(result.data);
+    axios.get("https://jsonplaceholder.typicode.com/todos/1").then((result) => {
+      this.result = result.data;
     })
   }
 };
 </script>
-
-<style scoped lang="scss">
-</style>
