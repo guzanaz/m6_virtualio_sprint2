bf0c0ae7772f42a1263f3732bc957369483be768
Daniela <daniela.gallardo.reyes@gmail.com>
2022-02-14 16:26:28 +0100

    Update pruebaAPI.vue
    ruta correcta para visualizar las máquinas localhost/api/VirtualMachine

--- src/views/pruebaAPI.vue
+++ src/views/pruebaAPI.vue
@@ -1,18 +1,9 @@
 <template>
-  <div v-if="result" class="content">
-    <p>User ID: {{result.user_id}}</p>
-    <p>Title: {{result.Name}}</p>
-    <p>OS: {{result.OS}}</p>
-    <p>Version: {{result.Version}}</p>
-    <p>Ram size: {{result.Ram_size}}</p>
-    <p>Disk capacity: {{result.Disk_capacity}}</p>
-    <p>Description: {{result.Description}}</p>
-    <p>Created at: {{result.created_at}}</p>
-    <p>Updated at: {{result.updated_at}}</p>
-    <p>Power on: {{result.Power_on}}</p>
-    <br>
-    <br>
+
+  <div>
+    <div v-for="(item, index) in result" :key="index">{{ index }}. {{ item }}</div>
   </div>
+
 </template>
 
 <script>
@@ -22,7 +13,7 @@ export default {
     result: null
   }),
   created() {
-    axios.get("http://127.0.0.1:8000/VirtualMachine/1").then((result) => {
+    axios.get("http://127.0.0.1:8000/api/VirtualMachine").then((result) => {
       this.result = result.data;
     })
   }
