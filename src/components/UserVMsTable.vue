503d1f6378d1e02e2e09216a1a77ca844b62c7a2
Daniela <daniela.gallardo.reyes@gmail.com>
2022-04-20 16:27:57 +0200

    intentos data table

--- src/components/UserVMsTable.vue
+++ src/components/UserVMsTable.vue
@@ -1,117 +1,97 @@
 <template>
   <div>
-      <b-card
-        bg-variant="white"
-        text-variant="dark"
-        class="border-0 shadow my-5"
-      >
-        <h4>Las mevas màquines</h4>
-        <!-- bootstrap-vue table -->
-        <b-row align-h="end">
-          <b-col md="3" class="pb-3">
-            <b-form-input
-              v-model="filter"
-              type="search"
-              placeholder="Cerca"
-            ></b-form-input>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col>
-            <b-table
-              responsive
-              outlined
-              striped
-              :items="items"
-              :filter="filter"
-              :per-page="perPage"
-              :current-page="currentPage"
-              :fields="fields"
-            >
-              <template v-slot:cell(edit)="">
-                <b-button id="show-btn" @click="showModal" variant="dark">
-                  <b-icon icon="gear-fill" aria-hidden="true"></b-icon>
-                </b-button>
-                <!-- modal pop up for editing virtual machine -->
-                <b-modal
-                  ref="modal"
-                  hide-header
-                  hide-footer
-                  title="Editant màquina Virtual"
-                  size="lg"
-                  @show="resetModal"
-                  @hidden="resetModal"
-                  @ok="handleOk"
-                >
-                  <!-- grupo para editar nombre de la máquina virtual -->
-                  <b-container fluid="sm">
-                    <b-row>
-                      <b-col cols="4"> </b-col>
-                      <b-col cols="8">
-                        <b-container fluid>
-                          <b-row class="my-1" v-for="type in types" :key="type">
-                            <label :for="`type-${type}`"
-                              >Type {{ type }}:</label
-                            >
-                            <b-form-input
-                              :id="`type-${type}`"
-                              :type="type"
-                            ></b-form-input>
-                          </b-row>
-                        </b-container>
-                      </b-col>
-                    </b-row>
-                  </b-container>
-                </b-modal>
-              </template>
-              <template v-slot:cell(delete)="data">
-                <b-button variant="danger" @click="deleteItem(data.item.id)">
-                  <b-icon icon="trash-fill" aria-hidden="true"></b-icon>
-                </b-button>
-              </template>
-            </b-table>
-            <b-pagination
-              v-model="currentPage"
-              :total-rows="rows"
-              :per-page="perPage"
-            ></b-pagination>
-          </b-col>
-        </b-row>
-      </b-card>
+    <b-card bg-variant="white" text-variant="dark" class="border-0 shadow my-5">
+      <h4>Las mevas màquines</h4>
+      <!-- bootstrap-vue table -->
+      <b-row align-h="end">
+        <b-col md="3" class="pb-3">
+          <b-form-input
+            v-model="filter"
+            type="search"
+            placeholder="Cerca"
+          ></b-form-input>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <b-table
+            responsive
+            outlined
+            striped
+            :items="items"
+            :filter="filter"
+            :per-page="perPage"
+            :current-page="currentPage"
+            :fields="fields"
+          >
+            <template v-slot:cell(edit)="">
+              <b-button id="show-btn" @click="showModal" variant="dark">
+                <b-icon icon="gear-fill" aria-hidden="true"></b-icon>
+              </b-button>
+              <!-- modal pop up for editing virtual machine -->
+              <b-modal
+                ref="modal"
+                hide-header
+                hide-footer
+                title="Editant màquina Virtual"
+                size="lg"
+                @show="resetModal"
+                @hidden="resetModal"
+                @ok="handleOk"
+              >
+                <!-- grupo para editar nombre de la máquina virtual -->
+                <b-container fluid="sm">
+                  <b-row>
+                    <b-col cols="4"> </b-col>
+                    <b-col cols="8">
+                      <b-container fluid>
+                        <b-row class="my-1" v-for="type in types" :key="type">
+                          <label :for="`type-${type}`">Type {{ type }}:</label>
+                          <b-form-input
+                            :id="`type-${type}`"
+                            :type="type"
+                          ></b-form-input>
+                        </b-row>
+                      </b-container>
+                    </b-col>
+                  </b-row>
+                </b-container>
+              </b-modal>
+            </template>
+            <template v-slot:cell(delete)="data">
+              <b-button variant="danger" @click="deleteItem(data.item.id)">
+                <b-icon icon="trash-fill" aria-hidden="true"></b-icon>
+              </b-button>
+            </template>
+          </b-table>
+          <b-pagination
+            v-model="currentPage"
+            :total-rows="rows"
+            :per-page="perPage"
+          ></b-pagination>
+        </b-col>
+      </b-row>
+    </b-card>
   </div>
 </template>
 
 <script>
-import Vm from"../apis/Vm";
+import Vm from "../apis/Vm";
 export default {
   mounted() {
-     //API Call
-   Vm.getAll().then(response =>{
-     //pushing data
+    //API Call
+    Vm.getAll().then((response) => {
+      //pushing data
       this.Vm = response.data;
       console.log(response.data);
-    })
+    });
   },
   data: () => ({
     return: {
-      Vm: '',
-      modalShow: false,
-      types: [
-        "text",
-        "number",
-        "email",
-        "password",
-        "search",
-        "url",
-        "tel",
-        "date",
-        "time",
-        "range",
-        "color",
-      ],
+      Vm: [],
       show: true,
     },
-    items: "[ ]",
+    items: [],
     fields: [
       {
         key: "vmid",
@@ -192,6 +172,20 @@ export default {
     perPage: 6,
     currentPage: 1,
   }),
+  modalShow: false,
+      types: [
+        "text",
+        "number",
+        "email",
+        "password",
+        "search",
+        "url",
+        "tel",
+        "date",
+        "time",
+        "range",
+        "color",
+      ],
   computed: {
     //validación nombre virtual Machine
     validateName() {
